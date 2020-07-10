b3f43f5a771c7cb6a36c30f4a12d38893883a672
nguyenhuuson <nguyenhuuson1997it@gmail.com>
2020-07-10 10:05:56 +0700

    model

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,7 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::apiresource('/product', 'ProductController');
+Route::group(['prefix' => 'product'], function(){
+	Route::apiresource('/{product}/review', 'ReviewController');
+});
